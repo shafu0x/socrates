db57359d6a8c5bd57ad10f1bea7ade0aa21cc155
shafu.eth <selfouly@gmail.com>
2024-03-19 00:36:50 +0100

    gitignore

---
+++ scraper/eth_global.py
@@ -0,0 +1,37 @@
+from web3 import Web3, EthereumTesterProvider
+
+RPC     = 'https://mainnet.infura.io/v3/d62b84418d9840c4911bbf30407ba7c8'
+ADDRESS = '0xD152f549545093347A162Dce210e7293f1452150'
+TX      = '0xdfe64868a256bfc7a2e4dc887983278b4801df9ffe0fc9989017ce1adb4521ba'
+
+ABI = [
+   {
+      "constant":False,
+      "inputs":[
+         {
+            "name":"recipients",
+            "type":"address[]"
+         },
+         {
+            "name":"values",
+            "type":"uint256[]"
+         }
+      ],
+      "name":"disperseEther",
+      "outputs":[
+         
+      ],
+      "payable":True,
+      "stateMutability":"payable",
+      "type":"function"
+   }
+]
+
+w3 = Web3(Web3.HTTPProvider(RPC))
+
+contract   = w3.eth.contract(address=ADDRESS, abi=ABI)
+tx         = w3.eth.get_transaction(TX)
+calldata   = tx['input']
+decoded    = contract.decode_function_input(calldata)
+recipients = decoded[1]['recipients']
+print(recipients)
