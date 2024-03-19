4735406236ac248926daf580e700c4712a51835c
shafu.eth <selfouly@gmail.com>
2024-03-19 07:48:19 +0100

    get all disperse addresses

--- scraper/setup.py
+++ scraper/setup.py
@@ -4,4 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv("../.env")
 
-w3 = Web3(Web3.HTTPProvider(os.environ.get('RPC')))
+CACHED        = True
+RPC           = os.environ.get('RPC')
+ETHERSCAN     = os.environ.get('ETHERSCAN_API_KEY')
+ETHERSCAN_API = "https://api.etherscan.io/api"
+
+w3 = Web3(Web3.HTTPProvider(RPC))
